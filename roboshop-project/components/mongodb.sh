af326aff9be6efd59b05dd465881b3a12b3a4cd6
vijay <bvsreddy703@gmail.com>
2022-03-14 10:50:50 +0530

    Updating shell-scripting

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -1,1 +1,8 @@
-echo Installing Mongodb
+source components/common.sh
+
+echo "Download Mongodb repo file"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+
+echo "Install MongoDB"
+yum install -y mongodb-org &>>$LOG_FILE
+
