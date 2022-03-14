f0866b901079415001654e9dc9488e535ed27692
vijay <bvsreddy703@gmail.com>
2022-03-15 02:11:15 +0530

    Updating shell-scripting

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -23,7 +23,7 @@ if [ -z "${PRIVATE_IP}" ]; then
     echo -e "\e[1;33m Security Group allow-all-ports does not exist"
     exit
    fi
-  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}" &>>$LOG
+  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}" 
   echo -e "\e[1m Instance Created\e[0m"
 else
   echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
