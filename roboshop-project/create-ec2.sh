7b4b95beecd9569b5bfe0d852c07edcdd99fee45
vijay <bvsreddy703@gmail.com>
2022-03-15 11:54:48 +0530

    Updating shell-scripting

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -36,7 +36,7 @@ echo '{
               "Changes": [{
               "Action": "UPSERT",
                           "ResourceRecordSet": {
-                                      "Name": "DNSNAME.roboshop.internal",
+                                      "Name": "DNSNAME",
                                       "Type": "A",
                                       "TTL": 300,
                                    "ResourceRecords": [{ "Value": "IPADDRESS"}]
@@ -44,5 +44,5 @@ echo '{
 }' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" >/tmp/record.json
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" --output text | grep roboshop.internal  | awk '{print $1}' | awk -F / '{print $3}')
   aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json --output text
-  
+
 
